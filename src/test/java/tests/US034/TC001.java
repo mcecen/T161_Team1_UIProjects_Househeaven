dd930d7edfbd11192d532d24b8113f26ec6a62f7
Yusuf Celal <yusufcelal34@gmail.com>
2025-07-09 22:35:25 +0300

    yusuf-22:33-09.07.2025

--- src/test/java/tests/US034/TC001.java
+++ src/test/java/tests/US034/TC001.java
@@ -1,10 +1,23 @@
 package tests.US034;
 
 
-public class TC034 {
-    public static void main(String[] args) {
-        System.out.println("Merhaba arkadaşlar ben Yusuf");
-        System.out.println("US034 testini yazıyorum");
-    }
+public class TC001 {
+
+//        Admin dashboard’a giriş yapmak
+//        Kullanıcı tarayıcı sayfasını açar.
+//        Kullanıcı tarayıcı arama çubuğuna tıklar.
+//        Kullanıcı arama çubuğuna https://qa.hauseheaven.com/admin/login adresini yazar.
+//        Kullanıcı arama çubuğuna yazılan URL için arama yapar.(Enter kullanılabilir)
+//        Kullanıcı HOUSE HEAVEN admin login anasayfasını görür.
+//        Kullanıcı email/username text alanını görür.
+//        Kullanıcı email/username text alanını tıklar.
+//        Kullanıcı email/username text alanına "yusufcelal_admin" verisini girer.
+//        Kullanıcı Password text alanını görür.
+//        Kullanıcı Password text alanını tıklar.
+//        Kullanıcı Password text alanına 021924 verisini girer.
+//        Kullanıcı Sign In butonunu görür.
+//        Kullanıcı Sign In butonunu tıklar.
+//        Sign in butonuna tıklama ile https://qa.hauseheaven.com/admin sayfasına erişebildiği görülür.
+//        Kullanıcı Admin dashboard anasayfasının açıldığı görür.
 
 }
---
+++ src/test/java/tests/US034/TC002.java
@@ -0,0 +1,38 @@
+package tests.US034;
+
+
+public class TC002 {
+
+//        Admin olarak, Real Estate başlığının altındaki menülerin görünür olmasını istiyorum.
+//        Admin kullanıcı https://qa.hauseheaven.com/admin/ adresi görüntülendiğini görür
+//        Kayıtlı kullanıcı dashboard anasayfasının açıldığı görür.
+//        Kayıtlı kullanıcı dashboard anasayfasının Sol menü Real Estate menüsü görünür.
+//        Kayıtlı kullanıcı olarak bu başlık altındaki tüm alt menüler Properties, Features, Facilities, Categories, Types görünür olduğu görüntülenmektedir.
+//        Kayıtlı kullanıcı olarak Propertiestıklandığında geçerli URL https://qa.hauseheaven.com/admin/real-estate/properties görüntülenmektedir.
+//        Kayıtlı kullanıcı olarak bu başlık altındaki Properties sayfasına tıklandığında Properties  Bulk Actions, Filters ve Serach, Create, Export, Reload ve girilmiş kayıtlar görüntülenmektedir.
+//        Kayıtlı kullanıcı olarak Bulk Actions tıklanınca Delete menüsü çıktığı görüntülenmektedir.
+//        Kayıtlı kullanıcı olarak page down yapınca sayfa içinde kaç satır listelenmeli seçeneği görüntülenmektedir.
+//        Kayıtlı kullanıcı olarak page down yapınca  "Show from 1 to 10 in 43 records" şeklinde kayıt sayısı ve görüntü satırı ve "« Previous1 2 3 4 5 6 Next »" işlmleri görüntülenmektedir.
+//        Kayıtlı kullanıcı olarak Features tıklandığında geçerli URL https://qa.hauseheaven.com/admin/real-estate/property-features görüntülenmektedir.
+//        Kayıtlı kullanıcı olarak bu başlık altındaki Features sayfasına tıklandığında Bulk Actions, Filters ve Serach, Create, Reload ve girilmiş kayıtlar görüntülenmektedir.
+//        Kayıtlı kullanıcı olarak Bulk Actions tıklanınca Delete menüsü çıktığı görüntülenmektedir.
+//        Kayıtlı kullanıcı olarak page down yapınca sayfa içinde kaç satır listelenmeli seçeneği görüntülenmektedir.
+//        Kayıtlı kullanıcı olarak page down yapınca  "Show from 1 to 10 in 43 records" şeklinde kayıt sayısı ve görüntü satırı ve "« Previous1 2 3 4 5 6 Next »" işlmleri görüntülenmektedir.
+//        Kayıtlı kullanıcı olarak Facilities tıklandığında geçerli URL https://qa.hauseheaven.com/admin/real-estate/facilities görüntülenmektedir.
+//        Kayıtlı kullanıcı olarak bu başlık altındaki Facilities sayfasına tıklandığında Bulk Actions, Filters ve Serach, Create, Reload ve girilmiş kayıtlar görüntülenmektedir.
+//        Kayıtlı kullanıcı olarak Bulk Actions tıklanınca Delete menüsü çıktığı görüntülenmektedir.
+//        Kayıtlı kullanıcı olarak page down yapınca sayfa içinde kaç satır listelenmeli seçeneği görüntülenmektedir.
+//        Kayıtlı kullanıcı olarak page down yapınca  "Show from 1 to 10 in 43 records" şeklinde kayıt sayısı ve görüntü satırı ve "« Previous1 2 3 4 5 6 Next »" işlmleri görüntülenmektedir.
+//        Kayıtlı kullanıcı olarak Categories tıklandığında geçerli URL https://qa.hauseheaven.com/admin/real-estate/categories görüntülenmektedir.
+//        Kayıtlı kullanıcı olarak bu başlık altındaki Categories sayfasına tıklandığında Bulk Actions, Filters ve Serach, Create, Reload ve girilmiş kayıtlar görüntülenmektedir.
+//        Kayıtlı kullanıcı olarak Bulk Actions tıklanınca Delete menüsü çıktığı görüntülenmektedir.
+//        Kayıtlı kullanıcı olarak page down yapınca sayfa içinde kaç satır listelenmeli seçeneği görüntülenmektedir.
+//        Kayıtlı kullanıcı olarak page down yapınca  "Show from 1 to 10 in 43 records" şeklinde kayıt sayısı ve görüntü satırı ve "« Previous1 2 3 4 5 6 Next »" işlmleri görüntülenmektedir.
+//        Kayıtlı kullanıcı olarak Types tıklandığında geçerli URL https://qa.hauseheaven.com/admin/real-estate/types görüntülenmektedir.
+//        Kayıtlı kullanıcı olarak bu başlık altındaki Types sayfasına tıklandığında Bulk Actions, Filters ve Serach, Create, Reload ve girilmiş kayıtlar görüntülenmektedir.
+//        Kayıtlı kullanıcı olarak Bulk Actions tıklanınca Delete menüsü çıktığı görüntülenmektedir.
+//        Kayıtlı kullanıcı olarak page down yapınca sayfa içinde kaç satır listelenmeli seçeneği görüntülenmektedir.
+//        Kayıtlı kullanıcı olarak page down yapınca  "Show from 1 to 10 in 43 records" şeklinde kayıt sayısı ve görüntü satırı ve "« Previous1 2 3 4 5 6 Next »" işlmleri görüntülenmektedir.
+
+
+}
