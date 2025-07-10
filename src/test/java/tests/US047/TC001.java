f5bc6635436bdb7b02b61eaa998f7e79a15b1bc7
mcecen <mustafacecen60@gmail.com>
2025-07-10 22:52:30 +0300

    10.07.2025_US047_TC001_TC002_US051_US052

--- src/test/java/tests/US047/TC001.java
+++ src/test/java/tests/US047/TC001.java
@@ -1,4 +1,75 @@
 package tests.US047;
 
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.CennetHauseheavenAdminPages;
+import utilities.ConfigReader;
+import utilities.Driver;
+import utilities.ReusableMethods;
+
 public class TC001 {
+
+    /*
+The user opens the browser page.
+The user clicks on the browser search bar.
+The user types https://qa.hauseheaven.com/admin/login into the search bar.
+The user searches for the URL typed into the search bar. (Enter can be used)
+The user sees the HOUSE HEAVEN admin login homepage.
+The user sees the email/username text field.
+The user clicks on the email/username text field.
+The user enters "mustafacecen_admin" into the email/username text field.
+The user sees the Password text field.
+The user clicks on the Password text field.
+The user enters 021924 into the Password text field.
+The user sees the Sign In button.
+The user clicks on the Sign In button.
+By clicking on the Sign In button, it is seen that the https://qa.hauseheaven.com/admin page can be accessed.
+The user sees that the Admin dashboard homepage opens.
+The user sees the Packages category from the left menu panel.
+The user clicks on the Packages category from the left menu panel.
+When the user clicks on the package category, the user sees that she can access the page https://qa.hauseheaven.com/admin/real-estate/packages.
+Create butonuna tıklanır ve https://qa.hauseheaven.com/admin/real-estate/packages/create sayfasına ulaşıldığı görülür.
+Name text alanına "deneme paket" yazılır.
+Status alanı "Published" olarak seçilir.
+Price alanına 100 değeri girilir
+Percent save alanına 1, number of listings alanına 1 değeri girilir.
+Save butonuna tıklanır ve created successfully başarılı mesajı görülür.
+Tarayıcı sayfasında geri gidilir ve oluşturulan kayıt kontrol edilir.
+edit butonuna tıklanır
+
+     */
+    @Test
+    public void test01(){
+
+        Driver.getDriver().get(ConfigReader.getProperty("adminUrl"));
+        String expectedUrlIcerik = "hauseheaven";
+        String actualUrlIcerik = Driver.getDriver().getCurrentUrl();
+
+        Assert.assertTrue(actualUrlIcerik.contains((expectedUrlIcerik)));
+
+        CennetHauseheavenAdminPages hauseheavenAdminPages = new CennetHauseheavenAdminPages();
+        Assert.assertTrue(hauseheavenAdminPages.emailKutusu.isDisplayed());
+
+        ReusableMethods.bekle(3);
+        hauseheavenAdminPages.emailKutusu.click();
+        hauseheavenAdminPages.emailKutusu.sendKeys(ConfigReader.getProperty("admin-mustafa"));
+
+        ReusableMethods.bekle(3);
+        hauseheavenAdminPages.passwordKutusu.click();
+        hauseheavenAdminPages.passwordKutusu.sendKeys(ConfigReader.getProperty("admin-mustafa-password"));
+
+        hauseheavenAdminPages.signInButton.click();
+
+        Assert.assertTrue(hauseheavenAdminPages.packageMenuAlanı.isDisplayed());
+
+        hauseheavenAdminPages.packageMenuAlanı.click();
+
+        String expectedPackageSayfaUrl="https://qa.hauseheaven.com/admin/real-estate/packages";
+        String actualPackageSayfaUrl= Driver.getDriver().getCurrentUrl();
+        Assert.assertEquals(expectedPackageSayfaUrl,actualPackageSayfaUrl);
+
+        Driver.quitDriver();
+
+
+    }
 }
---
+++ src/test/java/tests/US047/TC002.java
@@ -0,0 +1,75 @@
+package tests.US047;
+
+import com.github.javafaker.Faker;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.CennetHauseheavenAdminPages;
+import utilities.ConfigReader;
+import utilities.Driver;
+import utilities.ReusableMethods;
+
+public class TC002 {
+
+    @Test
+    public void test01(){
+
+        Driver.getDriver().get(ConfigReader.getProperty("adminUrl"));
+        String expectedUrlIcerik = "hauseheaven";
+        String actualUrlIcerik = Driver.getDriver().getCurrentUrl();
+
+        Assert.assertTrue(actualUrlIcerik.contains((expectedUrlIcerik)));
+
+        CennetHauseheavenAdminPages hauseheavenAdminPages = new CennetHauseheavenAdminPages();
+        Assert.assertTrue(hauseheavenAdminPages.emailKutusu.isDisplayed());
+
+        ReusableMethods.bekle(2);
+        hauseheavenAdminPages.emailKutusu.click();
+        hauseheavenAdminPages.emailKutusu.sendKeys(ConfigReader.getProperty("admin-mustafa"));
+
+        hauseheavenAdminPages.passwordKutusu.click();
+        hauseheavenAdminPages.passwordKutusu.sendKeys(ConfigReader.getProperty("admin-mustafa-password"));
+
+        hauseheavenAdminPages.signInButton.click();
+
+        Assert.assertTrue(hauseheavenAdminPages.packageMenuAlanı.isDisplayed());
+
+        hauseheavenAdminPages.packageMenuAlanı.click();
+
+        String expectedPackageSayfaUrl="https://qa.hauseheaven.com/admin/real-estate/packages";
+        String actualPackageSayfaUrl= Driver.getDriver().getCurrentUrl();
+        Assert.assertEquals(expectedPackageSayfaUrl,actualPackageSayfaUrl);
+        ReusableMethods.bekle(2);
+
+        Assert.assertTrue(hauseheavenAdminPages.packageCreateButton.isDisplayed());
+
+        hauseheavenAdminPages.packageCreateButton.click();
+
+        String expectedPackageCreateSayfaUrl="https://qa.hauseheaven.com/admin/real-estate/packages/create";
+        String actualPackageCreateSayfaUrl= Driver.getDriver().getCurrentUrl();
+        Assert.assertEquals(expectedPackageCreateSayfaUrl,actualPackageCreateSayfaUrl);
+        ReusableMethods.bekle(2);
+
+        Faker faker= new Faker();
+
+        hauseheavenAdminPages.packageNameText.sendKeys(faker.book().title());
+        ReusableMethods.bekle(2);
+
+        hauseheavenAdminPages.packagePriceText.sendKeys(faker.number().numberBetween(1000, 9999) + "");
+        ReusableMethods.bekle(2);
+
+        hauseheavenAdminPages.packagePersentSaveText.sendKeys(faker.number().numberBetween(1, 9) + "");
+        ReusableMethods.bekle(2);
+
+        hauseheavenAdminPages.packageNumberOfListingText.sendKeys(faker.number().numberBetween(1000, 9999) + "");
+        ReusableMethods.bekle(2);
+
+        hauseheavenAdminPages.packageSaveButton.click();
+
+        String expectedSonucMesaji= "Created successfully";
+        String actualSonucMesaji= hauseheavenAdminPages.createdSeccussfullyMesaji.getText();
+        Assert.assertEquals(expectedPackageCreateSayfaUrl,actualSonucMesaji);
+        ReusableMethods.bekle(2);
+        Driver.quitDriver();
+
+    }
+}
