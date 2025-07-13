4f1ed6b32a3f0e34edb6642c2f1dba97be3266e6
Guga243 <kivanch98@gmail.com>
2025-07-13 21:33:22 +0200

    Kivanc-00.33-13.07.2025

--- src/test/java/tests/US006/TC002.java
+++ src/test/java/tests/US006/TC002.java
@@ -7,7 +7,6 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-import pages.HauseheavenAnasayfa;
 import pages.HauseheavenPage;
 import utilities.ConfigReader;
 import utilities.Driver;
