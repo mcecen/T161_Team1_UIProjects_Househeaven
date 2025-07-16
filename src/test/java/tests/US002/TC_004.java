3d3431d3622f678c8f7d5cc3e79b34a3e174d04e
tayyar11 <tayyarmithat11@gmail.com>
2025-07-16 21:46:56 +0200

    tayyar_16.07.2025_US2_3

--- src/test/java/tests/US002/TC_004.java
+++ src/test/java/tests/US002/TC_004.java
@@ -1,4 +1,40 @@
 package tests.US002;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import utilities.ConfigReader;
+import utilities.Driver;
+
 public class TC_004 {
+
+    @Test
+    public void userCanAccessHomePageAndTopMenus() throws InterruptedException {
+
+        // 1. WebDriver başlat
+        WebDriver driver = Driver.getDriver();
+
+        // 2. URL'ye git
+        driver.get(ConfigReader.getProperty("url"));
+
+        // 3. URL'nin doğru açıldığını doğrula
+        Assert.assertEquals(driver.getCurrentUrl(), ConfigReader.getProperty("url"), "URL eşleşmiyor!");
+
+        // 4. Home menüsünün görünür olduğunu kontrol et
+        WebElement logo = driver.findElement(By.className("logo"));
+        Assert.assertTrue(logo.isDisplayed(), "Logo görünmüyor!");
+
+        Thread.sleep(3000);
+        WebElement listingElement = driver.findElement(By.xpath("//a[normalize-space()='Listing']"));
+        listingElement.click();
+
+
+
+        // 6. Tarayıcıyı kapat
+        Driver.quitDriver();
+
+    }
+
 }
--- src/test/java/tests/US003/TC_002.java
+++ src/test/java/tests/US003/TC_002.java
@@ -1,4 +1,50 @@
 package tests.US003;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import utilities.ConfigReader;
+import utilities.Driver;
+
 public class TC_002 {
+
+
+    @Test
+    public void userCanAccessHomePageAndTopMenus() {
+
+        // 1. WebDriver başlat
+        WebDriver driver = Driver.getDriver();
+
+        // 2. URL'ye git
+        driver.get(ConfigReader.getProperty("url"));
+
+        // 3. URL'nin doğru açıldığını doğrula
+        Assert.assertEquals(driver.getCurrentUrl(), ConfigReader.getProperty("url"), "URL eşleşmiyor!");
+
+        // 4. Lokasyon menüsünün görünür olduğunu kontrol et
+        WebElement lokasyon = driver.findElement(By.className("input-with-icon"));
+        Assert.assertTrue(lokasyon.isDisplayed(), "lokasyon görünmüyor");
+
+        // 5. Min Price menüsünün görünür olduğunu kontrol et
+        WebElement minPriceElement = driver.findElement(By.xpath("//*[text()='Min Price']"));
+        Assert.assertTrue(minPriceElement.isDisplayed(), "Min Price görünmüyor");
+
+        // 6. Min Price menüsünün görünür olduğunu kontrol et
+        WebElement maxPriceElement = driver.findElement(By.xpath("//*[text()='Max Price']"));
+        Assert.assertTrue(maxPriceElement.isDisplayed(), "Max Price görünmüyor");
+
+        // 6. Min Price menüsünün görünür olduğunu kontrol et
+        WebElement propertyTypeElement = driver.findElement(By.xpath("//*[text()='Property Type']"));
+        Assert.assertTrue(propertyTypeElement.isDisplayed(), " propertyTypeElement görünmüyor");
+
+
+
+        // 7. Tarayıcıyı kapat
+        Driver.quitDriver();
+
+
+    }
 }
+
