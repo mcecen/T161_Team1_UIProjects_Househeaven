20113528cf5ceaa86623e590f4ca090a7b31e287
tayyar11 <tayyarmithat11@gmail.com>
2025-07-17 21:33:38 +0200

    tayyar_17.7.2025_US003

--- src/test/java/tests/US003/TC_003.java
+++ src/test/java/tests/US003/TC_003.java
@@ -1,4 +1,36 @@
 package tests.US003;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+import utilities.ConfigReader;
+import utilities.Driver;
+
 public class TC_003 {
+
+    @Test
+    public void userCanAccessHomePageAndTopMenus() {
+
+        // 1. WebDriver başlat
+        WebDriver driver = Driver.getDriver();
+
+        // 2. URL'ye git
+        driver.get(ConfigReader.getProperty("url"));
+
+        // 3. Arama alanını bul (örnek olarak class, id veya placeholder’a göre)
+        WebElement searchBox = driver.findElement(By.xpath("//input[@placeholder='Search for a location']"));
+
+        // 4. SearchBox Tıkla
+        searchBox.click();
+
+        // 5. SearchBox'a "Texas" yaz
+        searchBox.sendKeys("Texas");
+
+        // 6. Tarayiciyi kapat
+        Driver.quitDriver();
+
+
+
+    }
 }
